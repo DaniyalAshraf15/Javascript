e2f4330938a7b284c2117a5afefb876e6ce64389
Daniyal Ashraf <64550426+Rayyan15@users.noreply.github.com>
2022-08-16 17:43:23 +0500

    Update qno10.js
    Updated

--- qno10.js
+++ qno10.js
@@ -1,8 +1,15 @@
-var arr = [23, 21, 34, 36];
-var temp_arr = [...new Set(arr)].slice(0); //clone array
-console.log(temp_arr);
-
-var second_largest_value = temp_arr.sort()[temp_arr.length - 2];
-
-
-console.log('Second largest value',second_largest_value);
+function value(){
+    var arr = [23, 21, 34, 36,13,16,30,40];
+    var temp_arr = [...new Set(arr)].slice(0); //clone array
+    
+    debugger;
+    var second_largest_value = temp_arr.sort()[temp_arr.length - 2];
+    var second_smallest_value = temp_arr.sort()[1]
+    console.log(arr.sort());
+    
+    
+    console.log('Second largest value',second_largest_value);
+    console.log('Second smallest value',second_smallest_value)
+    
+}
+value();
